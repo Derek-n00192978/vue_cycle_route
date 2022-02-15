d167b36c3586b8c5fa3612889a9103a9cf57e55d
Derek Reid <n00192978@student.iadt.ie>
2022-02-15 15:05:19 +0000

    Details added to indepth page

--- src/pages/routes/Indepth.vue
+++ src/pages/routes/Indepth.vue
@@ -3,14 +3,34 @@
       <h3 class="bold">Three of the most popular types of recreational cycling are,</h3>
       <h3>Road cycling, Mountain Biking and Gravel spins.</h3>
       
-        <!-- <div class="card img-fluid" style="width:400px">-->
-            <!--<img class="card-img-top" src="..../components/images/backgroung1.jpg" alt="vue logo" style="100%">-->
-         <!--   <div class="card-img-overlay">
-              <h4 class="card-title">W200</h4>
-              <p class="card-text">The W200 takes place on the second Sunday in June</p>
-              <a herf="#" class="btn btn-primary">See Route</a>
-            </div>            
-        </div> -->
+        <div class="row">
+            
+            <div class="col-md-4">
+              <div class="thumbnail">
+                <h5 class="card-title">Road Cycling</h5>
+                <p class="card-text">Road cycling as the name suggest takes place on the road. Using a bike with turned down handle bars for safety.</p>
+                <a herf="#" class="btn btn-primary">Road Routes</a>
+              </div>
+            </div>    
+            <div class="col-md-4">
+              <div class="thumbnail">
+                <h5 class="card-title">Mountain Biking</h5>
+                <p class="card-text">Mountain bikes (MTB) are bike's built with a sturdy frame, wide handle bars, broad deep-treaded tyers and suspension on the front fork as a minimum requirement.</p>
+                <p class="card-text">These frames are built for riding all-terrian cycling and decending routes at speed. For addded comfort and better handling capabilities rear suspension can be obtained.</p>
+                <p class="card-text"></p>
+                <a herf="#" class="btn btn-primary">Mountain Routes</a>
+              </div>
+            </div>   
+            <div class="col-md-4">
+              <div class="thumbnail">
+                <h5 class="card-title">Gravel Grinding</h5>
+                <p class="card-text">Gravel bikes is very simular to a road bike with a few sutle changes. For one a gravel bike is usuall longer than a compact road bike for better stability for cycling along rough terrian.</p>
+                <p class="card-text">The bottom bracket is higher off the ground for better ground clearance over obsticals such as roots and bolders. The handlebars are wider for comfort.</p>
+                <p class="card-text">For safety reasons a gravel bike would have disk brakes as the conventional rim brakes would be unuseable once the rim went through a muddy puddle.</p>
+                <a herf="#" class="btn btn-primary">Gravel Route</a>
+              </div>
+            </div>          
+        </div>
       </div>  
       
 </template>
