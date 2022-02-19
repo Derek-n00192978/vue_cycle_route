560f13639e4ed51c274217f5c15c731c406aed4c
Derek Reid <n00192978@student.iadt.ie>
2022-02-19 11:56:56 +0000

    Google static map aip embedded into landing page

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="container">
       <h2 class="bold">Welcome to the Leinster Leisure Cycling Routes Webpage</h2>
-      
+      <img src="https://maps.googleapis.com/maps/api/staticmap?center=53.2491581,-6.4148686&zoom=10&size=640x640&maptype=terrain&key=AIzaSyBYhv1UydxYzxt8JnB0P8lxHK9zY-98Gt4" alt="leinster_map">
     
   </div>    
 </template>
 <script>
 // import axios from '@/config'
 export default {
-  name: "indepth",
+  name: "landing",
 };
 
 </script>
